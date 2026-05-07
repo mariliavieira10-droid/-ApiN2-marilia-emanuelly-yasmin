b8f781310840866d500ee6eceb38f7052ab13fb1
Marilia <marilia.vieira10@aluno.ifce.edu.br>
2026-05-07 11:57:29 -0300

    fiz um route

---
+++ routes/api.routes.js
@@ -0,0 +1,15 @@
+import {Router} from "express";
+import ApiController from "../controllers/api.controller.js";
+import verifyparamId from "../midllewares/verify.id.middleware.js";
+
+const router = Router();
+
+const controller = new ApiController();
+
+router.get("/info", (req, res)=> controller.getInfo(req, res));
+
+router.get("/info/:id", verifyParamId, (req, res, next) => 
+    controller.getInfoByParamId(req,res, next),
+);
+
+export {router};
