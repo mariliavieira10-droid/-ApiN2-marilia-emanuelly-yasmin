c517d84326f50d26d48cdf4332965e5d3ad3a6f9
Emanuelly <emanuelly.costa07@aluno.ifce.edu.br>
2026-05-08 08:46:58 -0300

    terminando routes

--- routes/app.js
+++ routes/app.js
@@ -1,1 +1,16 @@
-import {  } from "module";
+import {router as apiRouter} from "./routes/api.routes.js";
+import {mdebug} from "./middlewares/debug.middleware.js";
+import {notFound, errorHandler} from "./middlewares/errors.middleware.js";
+import {mcors} from "./middlewares/mcors.middleware.js";
+
+const app = express();
+
+app.use(mcors);
+app.use(express.json());
+app.use(mdebug);
+
+app.use("/api", apiRouter);
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
