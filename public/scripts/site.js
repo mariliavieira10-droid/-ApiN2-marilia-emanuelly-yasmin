4b1a78ad337fe18175bc21bca8a3a2a1a3d14bca
Marilia <marilia.vieira10@aluno.ifce.edu.br>
2026-05-14 11:25:26 -0300

    tentativa de ajeitar erro

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -1,9 +1,9 @@
 async function carregarDadosSite() {
-    const res = await fetch("/api/home");
-    const dados = await res.json();
+  const res = await fetch("/api/home");
+  const dados = await res.json();
 
-    const h1 = document.querySelector("#titulo");
-    h1.textContent = dados.titulo;
+  const h1 = document.querySelector("#titulo");
+  h1.textContent = dados.titulo;
 }
- 
-    window.onload = carregarDadosSite;
+
+window.onload = carregarDadosSite;
--- src/app.js
+++ src/app.js
@@ -6,23 +6,21 @@ import express from "express";
 import path from "node:path";
 
 const app = express();
+// Middlewares
+app.use(express.static(path.join(import.meta.dirname, "public")));
+app.use(express.json()); // Importante para rotas REST
 
 app.use(mcors);
 app.use(express.json());
 app.use(mdebug);
 
 app.use("/api", apiRouter);
-app.use(notFound);
-app.use(errorHandler);
+
 
 // Configurações do EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(import.meta.dirname, "views")); // Node.js 20.11+
 
-// --- ROTA DE VIEW (EJS) ---
-app.get("/home", (req, res) => {
-  res.render("home"); // Renderiza o esqueleto da página
-});
 // --- ROTA DE VIEW (EJS) ---
 app.get("/home", (req, res) => {
   res.render("home"); // Renderiza o esqueleto da página
@@ -37,8 +35,8 @@ app.get("/products", (req, res) => {
   res.render("products");
 });
 
-// Middlewares
-app.use(express.static(path.join(import.meta.dirname, "public")));
-app.use(express.json()); // Importante para rotas REST
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
