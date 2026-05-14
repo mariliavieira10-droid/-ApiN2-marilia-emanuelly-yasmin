1c3174b5145d61faa3795bd3ae4a8869fe08779d
Marilia <marilia.vieira10@aluno.ifce.edu.br>
2026-05-14 11:14:25 -0300

    terceiro passo completo

--- src/app.js
+++ src/app.js
@@ -15,10 +15,6 @@ app.use("/api", apiRouter);
 app.use(notFound);
 app.use(errorHandler);
 
-// Middlewares
-app.use(express.static(path.join(import.meta.dirname, "public")));
-app.use(express.json()); // Importante para rotas REST
-
 // Configurações do EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(import.meta.dirname, "views")); // Node.js 20.11+
@@ -27,5 +23,22 @@ app.set("views", path.join(import.meta.dirname, "views")); // Node.js 20.11+
 app.get("/home", (req, res) => {
   res.render("home"); // Renderiza o esqueleto da página
 });
+// --- ROTA DE VIEW (EJS) ---
+app.get("/home", (req, res) => {
+  res.render("home"); // Renderiza o esqueleto da página
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+app.get("/products", (req, res) => {
+  res.render("products");
+});
+
+// Middlewares
+app.use(express.static(path.join(import.meta.dirname, "public")));
+app.use(express.json()); // Importante para rotas REST
 
 export default app;
