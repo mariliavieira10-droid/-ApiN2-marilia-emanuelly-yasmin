c1645ebf69bc96a78bb948f1b8d794d8f33d274b
Yasmin <yasmin.pinheiro07@aluno.ifce.edu.br>
2026-05-21 10:39:25 -0300

    Passo 6

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,17 +1,14 @@
-export default class ProductsController {
-  constructor() {}
-
-  async getProducts(req, res, next) {
+    async getProductById(req, res, next) {
     try {
-      const products = [
-        {id: 1, nome: "Teclado", disponivel: true},
-        {id: 2, nome: "Mouse", disponivel: true},
-        {id: 3, nome: "Monitor", disponivel: false},
-        {id: 4, nome: "Caixa de Som", disponivel: false},
-      ];
-      res.json({productsList: products});
+      const {id} = req.params; // Captura o "id" da URL
+      const product = {id: 1, nome: "Teclado", disponivel: true}; // simulação de acesso ao BD
+
+      if (!product) {
+        return res.status(404).json({message: "Produto não encontrado"});
+      }
+
+      res.json(product);
     } catch (error) {
       next(error);
     }
   }
-}
